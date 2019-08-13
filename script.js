b96919b24f47f9c4c19cf0f51802953a122ce4fa
Keeper9 <jermaine.n.reid@gmail.com>
2019-08-13 14:01:19 -0400

    Additional 2

--- script.js
+++ script.js
@@ -3,23 +3,24 @@ let counter = 1;
 function addClass()
 {
     let value = document.forms['tester']['newelement'].value;
-    document.forms['tester']['newelement'].value = "";
-
+	document.forms['tester']['newelement'].value = "";
+	
     if(value != "")
     {
-        let parent = document.createElement('div');
+		let parent = document.createElement('div');
         let label = document.createElement('label');
         let input = document.createElement('input');
        
         input.name = "class" + counter;
         counter += 1;
         input.type = "text";
-        label.for = child.name;
-        label.innerHTML = value + ": "; 
-        parent.appendChild(label);
+        label.for = input.name;
+        label.innerHTML = value + ": ";
+		
+		parent.appendChild(label);
         parent.appendChild(input);
-
-        document.insertBefore(parent,document.forms['tester']['submit']);
+		
+        document.forms['tester'].insertBefore(parent,document.forms['tester']['submit']);
     }
 
 }
